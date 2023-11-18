409a78fb135c7e6817e987f40dd6b9b2c72534ae
Abdulhafiz <abdulhafizaderemi@gmail.com>
2023-11-18 22:19:37 +0100

    mongo issue fixed

--- db/connect.js
+++ db/connect.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose')
 
-const uri = 'mongodb://127.0.0.1:27017/searchapi'
-// const uri = 'mongodb+srv://abdulhafiz:UsLWiNFxhf669ZfW@nodeexpressprojects.f8r0bvb.mongodb.net/searchapi?retryWrites=true&w=majority'
-// mongodb://127.0.0.1:27017/taskmanager
 const connectDB = () => {
-    return mongoose.connect(uri)
+    return mongoose.connect(process.env.MONGO_URI)
 }
 
 module.exports = connectDB
